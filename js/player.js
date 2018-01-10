47e3cb25f76564a5d66e9148ee9f3ae81683c13b
Fencl Lukáš <fencl@brokertrust.cz>
2018-01-10 15:47:13 +0100

    working laning

--- js/player.js
+++ js/player.js
@@ -1,51 +1,43 @@
-﻿game.Player = me.Sprite.extend({
+﻿game.Player = me.Entity.extend({
     init: function () {
         var image = me.loader.getImage("car");
-
         var middlePosition = (me.game.viewport.width / 2 - image.width / 2) + 18;
         var offset = 50;
 
-
-        this._super(me.Sprite, "init", [
+        this._super(me.Entity, "init", [
             middlePosition,
             me.game.viewport.height / 2 - image.height - 20,
-            { image: image }
+            { image: image, width: image.width, height: image.height }
         ]);
-        this.velx = 500;
-        this.leftPosition = middlePosition - offset;
-        this.elapsed = 1.5;
-        this.rightPosition = middlePosition + offset;
+
+        this.leftLane = middlePosition - offset;
+        this.rightLane = middlePosition + offset;
+        this.middleLane = middlePosition;
+        this.currentLane = 0;
     },
     update: function (time) {
         this._super(me.Sprite, "update", [time]);
-        var oldX = this.pos.x;
 
         if (me.input.isKeyPressed("left")) {
-            var distance = Math.sqrt(Math.pow(this.leftPosition - oldX, 2));
-            var directionX = (this.leftPosition - oldX) / distance;
-            var moving = true;
-
-            if (moving == true) {
-                this.pos.x += (directionX * this.velx) / this.elapsed;
-                if (Math.sqrt(Math.pow(this.pos.x - oldX, 2)) >= distance) {
-                    this.pos.x = this.leftPosition + 1;
-                    moving = false;
-                }
-            }
+            this.currentLane -= 1;
+            console.log('current lane', this.currentLane);
         }
 
         if (me.input.isKeyPressed("right")) {
-            var distance = Math.sqrt(Math.pow(this.rightPosition - oldX, 2));
-            var directionX = (this.rightPosition - oldX) / distance;
-            var moving = true;
+            this.currentLane += 1;
+            console.log('current lane', this.currentLane);
+        }
 
-            if (moving == true) {
-                this.pos.x += (directionX * this.velx) / this.elapsed;
-                if (Math.sqrt(Math.pow(this.pos.x - oldX, 2)) >= distance) {
-                    this.pos.x = this.rightPosition + 1;
-                    moving = false;
-                }
-            }
+        switch (this.currentLane) {
+            case -1:
+                this.pos.x = this.leftLane;
+                break;
+            case 0:
+                this.pos.x = this.middleLane;
+                break;
+            case 1:
+                this.pos.x = this.rightLane;
+                break;
         }
 
         this.pos.x = this.pos.x.clamp(this.leftPosition, this.rightPosition);
